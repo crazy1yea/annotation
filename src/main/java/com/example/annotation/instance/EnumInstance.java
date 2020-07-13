38b9454c4195a8d3e41660c6e831798943cf24c1
yangy <yangyangd@mail.taiji.com.cn>
2020-07-13 17:28:09 +0800

    ThreadPoolExecutor

---
+++ src/main/java/com/example/annotation/instance/EnumInstance.java
@@ -0,0 +1,33 @@
+package com.example.annotation.instance;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author yangy
+ */
+
+public enum EnumInstance {
+	/**
+	 * 实例
+	 */
+	INSTANCE;
+
+	private ThreadPoolExecutor executor;
+
+	private EnumInstance() {
+		executor = new ThreadPoolExecutor(
+				10,
+				15,
+				10,
+				TimeUnit.SECONDS,
+				new ArrayBlockingQueue<Runnable>(32),
+				Executors.defaultThreadFactory());
+	}
+
+	public ThreadPoolExecutor getInstance() {
+		return executor;
+	}
+}
